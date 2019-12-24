02937a0fdbc9dce8a7181a03eb9f52a3c7ae2fcf
Ritwick Bhargav <ritwickbhargav80@gmail.com>
2019-12-25 02:06:24 +0530

    few errors

--- config/auth.js
+++ config/auth.js
@@ -57,15 +57,31 @@ module.exports.allAuth = (req, res, next) => {
   }
 };
 
-module.exports.someAuth = (req, res, next) => {
+module.exports.restrictedAuth = (req, res, next) => {
   const token = req.header("x-auth-token");
   if (!token) {
-    if (
+    return res.status(401).json({ message: "Access denied!" });
+  } else {
+    const decodedPayload = jwt.verify(token, process.env.secret);
+    req.user = decodedPayload;
+    if (req.user.data.role === "admin") {
+      return next();
+    } else if (
       (req.user.data.role === "owner" && req.params.id === req.user.data._id) ||
       (req.user.data.role === "tenant" && req.params.id === req.user.data._id)
     ) {
       return next();
+    } else {
+      return res.status(401).json({ message: "Access denied!" });
     }
+  }
+};
+
+module.exports.someAuth = async (req, res, next) => {
+  const token = req.header("x-auth-token");
+  if (!token) {
+    let user = await DeletedUsers.findById(req.params.id);
+    if (user.email === req.params.email) return next();
     return res.status(401).json({ message: "Access denied!" });
   } else {
     const decodedPayload = jwt.verify(token, process.env.secret);
