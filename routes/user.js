e667bdf7c27b16169bd536869be5cc4577738bf6
Ritwick Bhargav <ritwickbhargav80@gmail.com>
2020-01-14 14:28:17 +0530

    user_controller Done!

--- routes/user.js
+++ routes/user.js
@@ -29,7 +29,7 @@ router.get("/profile", allAuth, profile);
 router.get("/forgetpassword/:emailormobile", sendForgetEmail);
 router.post("/forgetpassword/:id/:email", forgetPassword);
 router.post("/update", allAuth, imgupload.upload.single("image"), updateUser);
-router.get("/delete/:id/:email", someAuth, deleteUser); //
+router.get("/delete/:id/:email", someAuth, deleteUser);
 router.post("/requestremoveban/:email", requestRemoveBan);
 router.get("/removeban/:id", adminAuth, removeUserBan);
 
